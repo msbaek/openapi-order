afb4bf3deb878a0882f26f718b8676ba7bc82937
msbaek <ms.baek@sk.com>
2021-07-26 12:58:07 +0900

    project created

---
+++ src/main/java/com/example/openapiorder/OpenapiOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapiorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openapiorder/OpenapiOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapiorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
